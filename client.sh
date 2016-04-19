820dc772cd83e44abdd9f83ed95d02106b61e750
yhoshua6 <yhoshua6@gmail.com>
2016-04-19 12:13:55 -0500

    cambio de ip

--- client.sh
+++ client.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #Josué Hernández Mateo
+DIS=0
 display "0.jpg" &
-while :
-do
-	#statements
-	nc -l $1 $2 > client
-	x=$(cat client)
-	case "$x" in
-		0) display -remote "0.jpg" ;;
-		1) display -remote "1.jpg" ;;
-		2) display -remote "2.jpg" ;;
-		3) display -remote "3.jpg" ;;
-		4) display -remote "4.jpg" ;;
-		5) display -remote "5.jpg" ;;
-		6) display -remote "6.jpg" ;;
-		7) display -remote "7.jpg" ;;
-		8) display -remote "8.jpg" ;;
-		9) display -remote "9.jpg" ;;
-		*) echo "default";;
-    esac
-done
+DIS=$!
+if [ $DIS -ne 0 ]; then
+	sleep 3
+	while :
+	do
+		#statements
+		#nc -l -p 8000  > client
+		nc $1 8000 > client
+		x=$(cat client)
+		case "$x" in
+			0) display -remote "0.jpg" ;;
+			1) display -remote "1.jpg" ;;
+			2) display -remote "2.jpg" ;;
+			3) display -remote "3.jpg" ;;
+			4) display -remote "4.jpg" ;;
+			5) display -remote "5.jpg" ;;
+			6) display -remote "6.jpg" ;;
+			7) display -remote "7.jpg" ;;
+			8) display -remote "8.jpg" ;;
+			9) display -remote "9.jpg" ;;
+	    esac
+	done
+fi
--- server.sh
+++ server.sh
@@ -6,6 +6,7 @@ while :
 do
 	#statements
 	echo $(date +%T | awk '{print substr($1,8,1)}') > server
-	nc -q 0 $1 $2 < server
+	#nc 127.0.0.1 8000 < server
+	nc -l -p 8001 < server
 	sleep 1
 done
